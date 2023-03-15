ce7fc41333352507698306e049fef839a254d2b4
Lebohang Bernard <lbmotseki@gmail.com>
2023-03-15 06:47:42 +0200

    Update on progress

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
 function AboutUsPage() {
@@ -6,7 +7,7 @@ function AboutUsPage() {
     <div>
       <Navbar/>
      
-      <section id="cta1" class="bg-neutral-100  bg-[url('../public/img/about.svg')] h-[29.75rem] " > 
+      <section id="cta1" class="bg-neutral-100  bg-[url('../img/about.svg')] h-[29.75rem] " > 
 
         <div
         class="container flex flex-col items-center justify-between px-6 py-24 mx-auto space-y-12  md:py-12 md:flex-col md:space-y-10"
@@ -39,7 +40,7 @@ function AboutUsPage() {
 
         {/* class="bg-brightRed  bg-[url('../img/about.svg')] h-[29.75rem]" */}
         <div class="flex flex-col space-y-12 md:w-1/2">
-          <div  class="bg-brightRed  bg-[url('../public/img/about_section1.svg')] h-[20rem]">
+          <div  class="bg-brightRed  bg-[url('../img/about_section1.svg')] h-[20rem]">
           {/* <h2 class="max-w-md text-4xl font-bold text-center md:text-left">
             What's different about Manage?
           </h2>
@@ -50,7 +51,7 @@ function AboutUsPage() {
           </p> */}
           </div>
 
-          <div  class="bg-brightRed  bg-[url('../public/img/about_section2.svg')] h-[20rem]">
+          <div  class="bg-brightRed  bg-[url('../img/about_section2.svg')] h-[20rem]">
           {/* <h2 class="max-w-md text-4xl font-bold text-center md:text-left">
             What's different about Manage?
           </h2> */}
@@ -567,89 +568,7 @@ function AboutUsPage() {
       </div>
     </section>
 
-    {/* <!-- Footer --> */}
-    <footer class="bg-veryDarkBlue">
-      {/* <!-- Flex Container --> */}
-      <div
-        class="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0"
-      >
-        {/* <!-- Logo and social links container --> */}
-        <div
-          class="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start"
-        >
-          <div class="mx-auto my-6 text-center text-white md:hidden">
-            Copyright &copy; 2022, All Rights Reserved
-          </div>
-          {/* <!-- Logo --> */}
-          <div>
-            <img src="img/logo-white.svg" class="h-8" alt="" />
-          </div>
-          {/* <!-- Social Links Container --> */}
-          <div class="flex justify-center space-x-4">
-            {/* <!-- Link 1 --> */}
-            <a href="#">
-              <img src="img/icon-facebook.svg" alt="" class="h-8" />
-            </a>
-            {/* <!-- Link 2 --> */}
-            <a href="#">
-              <img src="img/icon-youtube.svg" alt="" class="h-8" />
-            </a>
-            {/* <!-- Link 3 --> */}
-            <a href="#">
-              <img src="img/icon-twitter.svg" alt="" class="h-8" />
-            </a>
-            {/* <!-- Link 4 --> */}
-            <a href="#">
-              <img src="img/icon-pinterest.svg" alt="" class="h-8" />
-            </a>
-            {/* <!-- Link 5 --> */}
-            <a href="#">
-              <img src="img/icon-instagram.svg" alt="" class="h-8" />
-            </a>
-          </div>
-        </div>
-        {/* <!-- List Container --> */}
-        <div class="flex justify-around space-x-32">
-          <div class="flex flex-col space-y-3 text-white">
-            <a href="#" class="hover:text-brightRed">Home</a>
-            <a href="#" class="hover:text-brightRed">Pricing</a>
-            <a href="#" class="hover:text-brightRed">Products</a>
-            <a href="#" class="hover:text-brightRed">About</a>
-          </div>
-          <div class="flex flex-col space-y-3 text-white">
-            <a href="#" class="hover:text-brightRed">Careers</a>
-            <a href="#" class="hover:text-brightRed">Community</a>
-            <a href="#" class="hover:text-brightRed">Privacy Policy</a>
-          </div>
-        </div>
-
-        {/* <!-- Input Container --> */}
-        <div class="flex flex-col justify-between">
-          <form>
-            <div class="flex space-x-3">
-              <input
-                type="text"
-                class="flex-1 px-4 rounded-full focus:outline-none"
-                placeholder="Updated in your inbox"
-              />
-              <button
-                class="px-6 py-2 text-white rounded-full bg-brightRed hover:bg-brightRedLight focus:outline-none"
-              >
-                Go
-              </button>
-            </div>
-          </form>
-          <div class="hidden text-white md:block">
-            Copyright &copy; 2022, All Rights Reserved
-          </div>
-        </div>
-      </div>
-    </footer>
-
-    {/* <script src="js/script.js"></script>
-  </body>
-</html> */}
-
+    <Footer/>
 
 
     </div>
