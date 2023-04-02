0e3f8106831dfdad7525a6182643526d53766726
Lebohang Bernard <lbmotseki@gmail.com>
2023-04-02 11:23:06 +0200

    Update wallet

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  define: {
+    "global": {},
+    'process.env': {},
+},
+  plugins: [react(), nodePolyfills()],
 })
+
+
+
