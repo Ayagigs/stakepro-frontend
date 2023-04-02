da496bb66a0678b59dbd561c6b828144963c64e7
Lebohang Bernard <lbmotseki@gmail.com>
2023-04-02 21:05:09 +0200

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,84 @@ import logo from '../assets/stakepro_log.svg';
 import percon_icon from '../assets/person.svg';
 import UserDashboardHome from '../dashboard/HomeDashboard/UserDashboardHome';
 
+//Motseki start
+// import logo from "./logo.svg";
+// import "./App.css";
+import Web3Modal from "web3modal";
+// import { ethers } from "ethers";
+// import { useState } from "react";
+import WalletConnect from "@walletconnect/web3-provider";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+
+// function App() {
+  // console.log(process.env.INFURA_ID)
+  // const providerOptions = {
+  //   binancechainwallet: {
+  //     package: true,
+  //   },
+  //   walletconnect: {
+  //     package: WalletConnect, // required
+  //     options: {
+  //       infuraId:  process.env.INFURA_ID// required
+  //     }
+  //   },
+  
+  //   coinbasewallet: {
+  //     package: CoinbaseWalletSDK, // Required
+  //     options: {
+  //       appName: "Coinbase", // Required
+  //       infuraId: process.env.INFURA_ID, // Required
+  //       chainId: 4, //4 for Rinkeby, 1 for mainnet (default)
+  //     },
+  //   },
+  // };
+
+  // const web3Modal = new Web3Modal({
+  //   network: "rinkeby",
+  //   theme: "light", // optional, 'dark' / 'light',
+  //   cacheProvider: false, // optional
+  //   providerOptions, // required
+  // });
+
+  // const [connectedAccount, setConnectedAccount] = useState("");
+
+  // const connectWeb3Wallet = async () => {
+  //   try {
+  //     const web3Provider = await web3Modal.connect();
+  //     const library = new ethers.providers.Web3Provider(web3Provider);
+  //     const web3Accounts = await library.listAccounts();
+  //     setConnectedAccount(web3Accounts[0]);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // const disconnectWeb3Modal = async () => {
+  //   await web3Modal.clearCachedProvider();
+  //   setConnectedAccount("");
+  // };
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <br />
+
+//         {connectedAccount && <p>Connected to ${connectedAccount}</p>}
+
+//         {!connectedAccount ? (
+//           <button onClick={connectWeb3Wallet}>Connect Wallet</button>
+//         ) : (
+//           <button onClick={disconnectWeb3Modal}>Disconnect</button>
+//         )}
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+//Motseki end
+
 
 
 const randomString = function (length) {
@@ -26,7 +104,54 @@ export default function Navbar() {
         const [flyerTwo, setFlyerTwo] = React.useState(false);
         const [flyerThree, setFlyerThree] = React.useState(false);
 
-
+        //Motseki start
+        console.log(process.env.INFURA_ID)
+        const providerOptions = {
+          binancechainwallet: {
+            package: true,
+          },
+          walletconnect: {
+            package: WalletConnect, // required
+            options: {
+              infuraId:  process.env.INFURA_ID// required
+            }
+          },
+        
+          coinbasewallet: {
+            package: CoinbaseWalletSDK, // Required
+            options: {
+              appName: "Coinbase", // Required
+              infuraId: process.env.INFURA_ID, // Required
+              chainId: 4, //4 for Rinkeby, 1 for mainnet (default)
+            },
+          },
+        };
+      
+        const web3Modal = new Web3Modal({
+          network: "rinkeby",
+          theme: "light", // optional, 'dark' / 'light',
+          cacheProvider: false, // optional
+          providerOptions, // required
+        });
+      
+        const [connectedAccount, setConnectedAccount] = useState("");
+      
+        const connectWeb3Wallet = async () => {
+          try {
+            const web3Provider = await web3Modal.connect();
+            const library = new ethers.providers.Web3Provider(web3Provider);
+            const web3Accounts = await library.listAccounts();
+            setConnectedAccount(web3Accounts[0]);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+      
+        const disconnectWeb3Modal = async () => {
+          await web3Modal.clearCachedProvider();
+          setConnectedAccount("");
+        };
+        //Motseki End
 
 
         const [msg, setMsg] = useState();
@@ -159,7 +284,7 @@ export default function Navbar() {
                       type="button"
                         className="group bg-[#FF6842] rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                        
-                        onClick={cryptoButton}
+                        onClick={connectWeb3Wallet}
                       >
                                 
                           <Link to={"/wallet"}
